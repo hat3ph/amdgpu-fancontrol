620a32a66552e2c71f69d90d47005667db7f43a0
hat3ph <88069788+hat3ph@users.noreply.github.com>
2022-05-05 20:23:15 +0800

    Add auto installation script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'error: Must be run as root!' >&2
+        exit 1
+fi
+
+# Copy files
+mkdir -p /opt/amdgpu-fancontrol
+cp amdgpu-fancontrol /opt/amdgpu-fancontrol/
+cp amdgpu-fancontrol.cfg /opt/amdgpu-fancontrol/
+mkdir -p /etc/systemd/system
+cp amdgpu-fancontrol.service /etc/systemd/system/
+
+# Enable & start service
+systemctl enable amdgpu-fancontrol.service
+systemctl start amdgpu-fancontrol.service
